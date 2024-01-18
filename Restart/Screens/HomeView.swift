8c6ac7229ab8cf45a8a4daeeb6a9292496d44937
Abhyuday Reddy Nandikonda <abhyudayreddy1534@gmail.com>
2024-01-17 19:59:42 -0600

    Basic structure for Launch screen before animations.

---
+++ Restart/Screens/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Restart
+//
+//  Created by Sravanthi Chinthireddy on 17/01/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ Restart/Screens/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  Restart
+//
+//  Created by Sravanthi Chinthireddy on 17/01/24.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
