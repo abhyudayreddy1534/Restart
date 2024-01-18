e48c8b30893d976674add14270c3ec6ce9913a19
Abhyuday Reddy Nandikonda <abhyudayreddy1534@gmail.com>
2024-01-17 19:22:23 -0600

    Initial Commit

---
+++ Restart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by Sravanthi Chinthireddy on 17/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Sravanthi Chinthireddy on 17/01/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
